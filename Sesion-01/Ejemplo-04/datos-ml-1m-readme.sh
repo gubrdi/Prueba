f5b65a129e7e763c14aa90603438a6a543ca4991
Alejandro <martin.alejandro.lopez.rojas@gmail.com>
2020-02-29 15:52:17 -0600

    archivos al cierre de febrero-alr

---
+++ Sesion-01/Ejemplo-04/datos-ml-1m-readme.sh
@@ -0,0 +1,6 @@
+mkdir Datos #crea directorio
+cd Datos # cambio de directorio
+# Se descarga archivo de texto
+curl -0 http://files.grouplens.org/datasets/movielens/ml-1m-README.txt
+ls # se listan los archivos
+
